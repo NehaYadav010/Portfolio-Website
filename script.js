1516d47b82f4294b5ca5df52c0c5dca0e43a38bf
NehaYadav010 <yadavneha594@gmail.com>
2024-06-21 16:06:05 +0530

    starting project

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".reveal")
+    .forEach(function(elem){
+        let spanParent = document.createElement("span");
+        let spanChild = document.createElement("span");
+
+        spanParent.classList.add("parent");
+        spanChild.classList.add("child");
+
+        spanChild.textContent = elem.textContent;
+        spanParent.appendChild(spanChild);
+
+        elem.innerHTML = "";
+        elem.appendChild(spanParent);
+    })
